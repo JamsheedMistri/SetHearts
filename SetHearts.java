34ae07c3ecd825ff74a5ac50796e64f1155d20ba
Mistri <jmistri7@gmail.com>
2015-12-13 12:26:18 -0800

    Update to v1.1
    - In the previous version, the health was scaled, meaning that if you set your hearts to 20, it would work, but something that would normally do two hearts would do four hearts, so it was simply aesthetic. Now, each individual heart acts as its own.
    - Fixed an internal error with hearts going above 1024.
    - Added compatibility with my new plugin.

--- SetHearts.java
+++ SetHearts.java
@@ -34,8 +34,13 @@ public class SetHearts extends JavaPlugin {
 				if (commandLabel.equalsIgnoreCase("sethearts")) {
 					if (args.length == 1) {
 						try {
-							p.setHealthScale(Double.parseDouble(args[0]) * 2);
-							p.sendMessage(prefix + "Set your hearts to " + heart + (Double.parseDouble(args[0])));
+							if (Double.parseDouble(args[0]) <= 1024) {
+								p.setHealthScale(Double.parseDouble(args[0]) * 2);
+								p.setMaxHealth(Double.parseDouble(args[0]) * 2);
+								p.setHealth(Double.parseDouble(args[0]) * 2);
+								p.sendMessage(prefix + "Set your hearts to " + heart + (Double.parseDouble(args[0])));
+							}
+							else p.sendMessage(prefix + error + args[0] + " is out of range. Maximum amount of hearts allowed is " + heart + "1024" + error + ".");
 						}
 						catch(NumberFormatException exception) {
 							p.sendMessage(prefix + error + args[0] + " is not a valid number.");
@@ -45,19 +50,25 @@ public class SetHearts extends JavaPlugin {
 					else if (args.length == 2) {
 						if (Bukkit.getPlayer(args[0]) != null) {
 							try {
-								Player target = Bukkit.getPlayer(args[0]);
-								target.setHealthScale(Double.parseDouble(args[1]) * 2);
-								p.sendMessage(prefix + "Set the amount of hearts of " + args[0] + " to " + heart + (Double.parseDouble(args[1])));
+								if (Double.parseDouble(args[1]) <= 1024) {
+									Player target = Bukkit.getPlayer(args[0]);
+									target.setHealthScale(Double.parseDouble(args[1]) * 2);
+									target.setMaxHealth(Double.parseDouble(args[1]) * 2);
+									target.setHealth(Double.parseDouble(args[1]) * 2);
+									p.sendMessage(prefix + "Set the amount of hearts of " + args[0] + " to " + heart + (Double.parseDouble(args[1])));
+								}
+								else p.sendMessage(prefix + error + args[1] + " is out of range. Maximum amount of hearts allowed is " + heart + "1024" + error + ".");
 							}
 							catch(NumberFormatException exception) {
-								p.sendMessage(prefix + error + args[0] + " is not a number.");
+								p.sendMessage(prefix + error + args[0] + " is not a valid number.");
 							}
 
 						}
 
 					}
 					else if (args.length == 0) {
-						p.sendMessage(prefix + "SetHearts v1.0 made by " + error + ChatColor.BOLD + "MISTRI\n" + prefix + "Usage: " + ChatColor.RED + "/sethearts [player] <amount>");
+						p.sendMessage(prefix + "SetHearts v1.1 made by " + ChatColor.GOLD + "Mistri");
+						p.sendMessage(ChatColor.RED + "Usage: " + ChatColor.GOLD + "/sethearts [player] <amount>");
 
 					}
 					else p.sendMessage(prefix + error + "Improper usage.\n" + prefix + "Correct usage: " + ChatColor.BLUE + "/sethearts [player] <amount>");
